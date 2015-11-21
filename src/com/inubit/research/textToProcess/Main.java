a25799e779335b4ea593b0d5fb33c69dbb034081
angelotti <angelotti72@gmail.com>
2015-11-21 17:50:36 +0200

    project imported to eclipse. changed the structure a bit (resources,lib). fresh download wordnet dict files because of runtime errors

---
+++ src/com/inubit/research/textToProcess/Main.java
@@ -0,0 +1,37 @@
+/**
+ * copyright
+ * Inubit AG
+ * Schoeneberger Ufer 89
+ * 10785 Berlin
+ * Germany
+ */
+package com.inubit.research.textToProcess;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import com.inubit.research.gui.Workbench;
+import com.inubit.research.gui.plugins.WorkbenchPlugin;
+
+/**
+ * @author ff
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Locale.getDefault());
+		TextToProcessPlugin _plugin = new TextToProcessPlugin();
+		ArrayList<WorkbenchPlugin> _plugins = new ArrayList<WorkbenchPlugin>();
+		_plugins.add(_plugin);
+		Workbench _w = new Workbench(true,_plugins);
+		_plugin.setWorkbench(_w);
+		_w.addPlugin(_plugin);
+		_w.setVisible(true);
+		_plugin.openFrame();
+	}
+
+}
